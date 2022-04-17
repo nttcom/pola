46b0066b0f899a9bc310081c87efabd3f02ee496
Motok1 <m.takenaka@ntt.com>
2022-04-17 11:28:47 +0000

    Refactoring

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -19,7 +19,7 @@ import (
 	"github.com/nttcom/pola/pkg/packet/pcep"
 )
 
-type lsp struct {
+type Lsp struct {
 	peerAddr     net.IP // 後々 router ID, router name などに変更したい
 	plspId       uint32
 	name         string
@@ -28,7 +28,7 @@ type lsp struct {
 
 type Server struct {
 	sessionList []*Session
-	lspList     []lsp
+	lspList     []Lsp
 	pb.UnimplementedPceServiceServer
 }
 
@@ -39,9 +39,10 @@ type PceOptions struct {
 
 func NewPce(o *PceOptions) error {
 	s := &Server{}
+	lspChan := make(chan Lsp)
 	// PCEP の Listen を開始する
 	go func() {
-		if err := s.Listen(o.PcepAddr, o.PcepPort); err != nil {
+		if err := s.Listen(o.PcepAddr, o.PcepPort, lspChan); err != nil {
 			fmt.Printf("PCEP listen Error\n")
 		}
 
@@ -53,16 +54,25 @@ func NewPce(o *PceOptions) error {
 		}
 
 	}()
-
+	ticker := time.NewTicker(time.Duration(10) * time.Second)
+	defer ticker.Stop()
 	// sessionList の表示
 	for {
-		s.printSessionList()
-		s.printLspList()
-		time.Sleep(10 * time.Second)
+		select {
+
+		case lsp := <-lspChan:
+			// 更新用に旧 LSP 情報を削除する
+			s.removeLsp(lsp)
+			s.lspList = append(s.lspList, lsp)
+		case <-ticker.C:
+			s.printSessionList()
+			s.printLspList()
+		}
+
 	}
 }
 
-func (s *Server) Listen(address string, port string) error {
+func (s *Server) Listen(address string, port string, lspChan chan Lsp) error {
 	// PCEP の listen を行う
 	var listenInfo strings.Builder
 	listenInfo.WriteString(address)
@@ -77,7 +87,7 @@ func (s *Server) Listen(address string, port string) error {
 	defer listener.Close()
 	sessionId := uint8(1)
 	for {
-		session := NewSession(sessionId)
+		session := NewSession(sessionId, lspChan)
 
 		fmt.Printf("%#v\n", s)
 		session.tcpConn, err = listener.Accept()
@@ -154,10 +164,10 @@ func (s *Server) printLspList() {
 	fmt.Printf("printLspList ************\n")
 	fmt.Printf("*\n")
 	for _, lsp := range s.lspList {
-		fmt.Printf("|- LSP Owner address %s\n", lsp.peerAddr.String())
-		fmt.Printf("|  LSP Name %s\n", lsp.name)
-		fmt.Printf("|  PLSP-ID %d\n", lsp.plspId)
-		fmt.Printf("|  lspObject %#v\n", lsp.pcrptMessage)
+		fmt.Printf("*- LSP Owner address %s\n", lsp.peerAddr.String())
+		fmt.Printf("*  LSP Name %s\n", lsp.name)
+		fmt.Printf("*  PLSP-ID %d\n", lsp.plspId)
+		fmt.Printf("*  lspObject %#v\n", lsp.pcrptMessage)
 	}
 	fmt.Printf("*\n")
 	fmt.Printf("*************************\n")
@@ -173,14 +183,13 @@ func (s *Server) removeSession(sessionId uint8) {
 	}
 }
 
-// func removeLsp(lspList []lsp, e lsp) []lsp {
-// 	// lspList から name, PLSP-ID, sessionAddr が一致するものを削除する
-// 	result := []lsp{}
-// 	for _, lsp := range lspList {
-// 		if lsp.name == e.name && lsp.plspId == e.plspId && lsp.peerAddr.Equal(e.peerAddr) {
-// 			continue
-// 		}
-// 		result = append(result, lsp)
-// 	}
-// 	return result
-// }
+func (s *Server) removeLsp(e Lsp) {
+	// lspList から name, PLSP-ID, sessionAddr が一致するものを削除する
+	for i, v := range s.lspList {
+		if v.name == e.name && v.plspId == e.plspId && v.peerAddr.Equal(e.peerAddr) {
+			s.lspList[i] = s.lspList[len(s.lspList)-1]
+			s.lspList = s.lspList[:len(s.lspList)-1]
+			break
+		}
+	}
+}
