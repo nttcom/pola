6eec91efbf2d3e2e5036606343b0f3e02203c3b5
Motok1 <m.takenaka@ntt.com>
2022-05-10 15:22:01 +0000

    Add test client for get server's state

---
+++ tools/grpc/go/client/test_show_lsp_list.go
@@ -0,0 +1,59 @@
+// Copyright (c) 2022 NTT Communications Corporation
+//
+// This software is released under the MIT License.
+// see https://github.com/nttcom/pola/blob/main/LICENSE
+
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+
+	"google.golang.org/grpc"
+
+	empty "github.com/golang/protobuf/ptypes/empty"
+	pb "github.com/nttcom/pola/api/grpc"
+)
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewPceServiceClient(conn)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	var empty empty.Empty
+	ret, err := c.GetLspList(ctx, &empty)
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+
+	for i, lsp := range ret.GetLsps() {
+		fmt.Printf("lsp(%d): \n", i)
+		sessionAddr := net.IP(lsp.GetPcepSessionAddr())
+		fmt.Printf("  sessionAddr: %s\n", sessionAddr.String())
+		fmt.Printf("  policyName: %s\n", lsp.GetPolicyName())
+		fmt.Printf("  path: ")
+
+		if len(lsp.GetLabels()) == 0 {
+			fmt.Printf("None \n")
+			continue
+		}
+		for j, label := range lsp.GetLabels() {
+			fmt.Printf("%d ", label.GetSid())
+			if j == len(lsp.GetLabels())-1 {
+				fmt.Printf("\n")
+			} else {
+				fmt.Printf("-> ")
+			}
+		}
+	}
+}
