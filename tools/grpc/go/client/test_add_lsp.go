b6398b391cddcc09cc29224ec1afc873a4ac79f1
Motok1 <m.takenaka@ntt.com>
2022-05-10 15:22:01 +0000

    Add test client for get server's state

--- tools/grpc/go/client/test_add_lsp.go
+++ tools/grpc/go/client/test_add_lsp.go
@@ -8,10 +8,7 @@ package main
 import (
 	"context"
 	"flag"
-	"fmt"
 	"log"
-	"net"
-	"strconv"
 	"time"
 
 	"google.golang.org/grpc"
@@ -27,96 +24,49 @@ func main() {
 	flag.Parse()
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
-		log.Fatalf("Can't connect: %v", err)
+		log.Fatalf("did not connect: %v", err)
 	}
 	defer conn.Close()
 	c := pb.NewPceServiceClient(conn)
 
-	var numOfLabels string
-	var sidStr string
-	var localAddrStr string
-	var sessionAddrStr string
-	var srcAddrStr string
-	var dstAddrStr string
-	var policyName string
-	var colorStr string
-
-	fmt.Printf("sessionAddr: ")
-	fmt.Scan(&sessionAddrStr)
-	fmt.Printf("%#v  \n", net.ParseIP(sessionAddrStr))
-	sessionAddr := net.ParseIP(sessionAddrStr)
-	if sessionAddr == nil {
-		log.Fatalf("Invalid LocalAddr\n")
-	}
-	fmt.Printf("srcAddr: ")
-	fmt.Scan(&srcAddrStr)
-	srcAddr := net.ParseIP(srcAddrStr)
-	if srcAddr == nil {
-		log.Fatalf("Invalid LocalAddr\n")
-	}
-	fmt.Printf("dstAddrStr: ")
-	fmt.Scan(&dstAddrStr)
-	dstAddr := net.ParseIP(dstAddrStr)
-	if dstAddr == nil {
-		log.Fatalf("Invalid LocalAddr\n")
-	}
-	fmt.Printf("policyName: ")
-	fmt.Scan(&policyName)
-
-	fmt.Printf("Color: ")
-	fmt.Scan(&colorStr)
-	color, err := strconv.Atoi(colorStr)
-	if err != nil {
-		log.Fatalf("Invalid color\n")
-	}
-
-	fmt.Printf("Number of labels?: ")
-	fmt.Scan(&numOfLabels)
-	numberOfLabels, err := strconv.Atoi(numOfLabels)
-	if err != nil {
-		log.Fatalf("Invalid input\n")
-	}
-
-	// Create Segment List
-	labels := []*pb.Label{}
-	fmt.Printf("Create Sid List\n")
-	for i := 0; i < numberOfLabels; i++ {
-
-		fmt.Printf("Sid: ")
-		fmt.Scan(&sidStr)
-		fmt.Printf("LocalAddr: ")
-		fmt.Scan(&localAddrStr)
-
-		sid, err := strconv.Atoi(sidStr)
-		if err != nil {
-			log.Fatalf("Invalid Sid\n")
-		}
-		localAddr := net.ParseIP(localAddrStr)
-		if localAddr == nil {
-			log.Fatalf("Invalid LocalAddr\n")
-		}
-		label := pb.Label{
-			Sid:    uint32(sid),
-			LoAddr: []byte(localAddr.To4()),
-		}
-		fmt.Printf("%#v\n", label)
-
-		labels = append(labels, &label)
-	}
-
 	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 	defer cancel()
 
+	labels := []*pb.Label{}
+	label1 := pb.Label{
+		Sid:    16003,
+		LoAddr: []byte{0x0a, 0xff, 0x00, 0x03},
+	}
+	labels = append(labels, &label1)
+
+	label2 := pb.Label{
+		Sid:    16001,
+		LoAddr: []byte{0x0a, 0xff, 0x00, 0x01},
+	}
+	labels = append(labels, &label2)
+
+	label3 := pb.Label{
+		Sid:    16003,
+		LoAddr: []byte{0x0a, 0xff, 0x00, 0x03},
+	}
+	labels = append(labels, &label3)
+
+	label4 := pb.Label{
+		Sid:    16001,
+		LoAddr: []byte{0x0a, 0xff, 0x00, 0x01},
+	}
+	labels = append(labels, &label4)
+
 	r, err := c.CreateLsp(ctx, &pb.LspData{
-		PcepSessionAddr: []byte(sessionAddr.To4()),
-		SrcAddr:         []byte(srcAddr.To4()),
-		DstAddr:         []byte(dstAddr.To4()),
+		PcepSessionAddr: []byte{10, 100, 0, 2},
+		SrcAddr:         []byte{10, 255, 0, 2},
+		DstAddr:         []byte{10, 255, 0, 1},
 		Labels:          labels,
-		Color:           uint32(color),
-		PolicyName:      policyName,
+		Color:           uint32(1),
+		PolicyName:      "test-color1-policy",
 	})
 	if err != nil {
-		log.Fatalf("CreateLsp error: %v", err)
+		log.Fatalf("could not greet: %v", err)
 	}
 	log.Printf("Success: %#v", r)
 }
