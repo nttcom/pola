eefc57fa615294045c1cc51c149ee8ea367f821f
Motok1 <m.takenaka@ntt.com>
2022-05-18 01:48:18 +0000

    Fix linter error

--- tools/grpc/go/client/show_lsp_list/test_show_lsp_list.go
+++ tools/grpc/go/client/show_lsp_list/test_show_lsp_list.go
@@ -14,6 +14,7 @@ import (
 	"time"
 
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 
 	empty "github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/nttcom/pola/api/grpc"
@@ -21,7 +22,7 @@ import (
 
 func main() {
 	flag.Parse()
-	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+	conn, err := grpc.Dial("localhost:50051", grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
