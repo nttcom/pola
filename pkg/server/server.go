73cb926d1d0530eb617c4cfd6a5f28082f239185
Motok1 <m.takenaka@ntt.com>
2022-10-11 10:27:14 +0000

    Refactoring server and Path calculation using TED

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -6,18 +6,15 @@
 package server
 
 import (
-	"context"
 	"net"
 	"strings"
 
 	"go.uber.org/zap"
 	grpc "google.golang.org/grpc"
 
-	"github.com/golang/protobuf/ptypes/empty"
 	pb "github.com/nttcom/pola/api/grpc"
 
 	"github.com/nttcom/pola/internal/pkg/table"
-	"github.com/nttcom/pola/pkg/packet/pcep"
 )
 
 type Lsp struct {
@@ -33,8 +30,7 @@ type Server struct {
 	sessionList []*Session
 	lspList     []Lsp
 	ted         *table.LsTed
-	pb.UnimplementedPceServiceServer
-	logger *zap.Logger
+	logger      *zap.Logger
 }
 
 type PceOptions struct {
@@ -85,7 +81,9 @@ func NewPce(o *PceOptions, logger *zap.Logger, tedElemsChan chan []table.TedElem
 	}()
 	// Start gRPC listen
 	go func() {
-		if err := s.grpcListen(o.GrpcAddr, o.GrpcPort); err != nil {
+		grpcServer := grpc.NewServer()
+		apiServer := NewAPIServer(s, grpcServer)
+		if err := apiServer.Serve(o.GrpcAddr, o.GrpcPort); err != nil {
 			s.logger.Panic("gRPC Listen Error", zap.Error(err), zap.String("server", "grpc"))
 		}
 	}()
@@ -130,138 +128,6 @@ func (s *Server) Listen(address string, port string, lspChan chan Lsp) error {
 	}
 }
 
-func (s *Server) grpcListen(address string, port string) error {
-	var listenInfo strings.Builder
-	listenInfo.WriteString(address)
-	listenInfo.WriteString(":")
-	listenInfo.WriteString(port)
-	s.logger.Info("gRPC Listen", zap.String("listenInfo", listenInfo.String()), zap.String("server", "grpc"))
-	grpcListener, err := net.Listen("tcp", listenInfo.String())
-	if err != nil {
-		return err
-	}
-	grpcServer := grpc.NewServer()
-
-	pb.RegisterPceServiceServer(grpcServer, s)
-	if err := grpcServer.Serve(grpcListener); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (s *Server) CreateLsp(ctx context.Context, lspData *pb.LspData) (*pb.LspStatus, error) {
-	s.logger.Info("Get request CreateSrPolicy API", zap.Any("SR Policy", lspData), zap.String("server", "grpc"))
-	pcepPeerAddr := net.IP(lspData.GetPcepSessionAddr())
-	if pcepSession := s.getSession(pcepPeerAddr); pcepSession != nil {
-		labels := []pcep.Label{}
-		for _, receivedLsp := range lspData.GetLabels() {
-			pcepLabel := pcep.Label{
-				Sid:    receivedLsp.GetSid(),
-				LoAddr: receivedLsp.GetLoAddr(),
-			}
-			labels = append(labels, pcepLabel)
-		}
-		if plspId := s.getPlspId(lspData); plspId != 0 {
-			s.logger.Info("plspId check", zap.Uint32("plspId", plspId), zap.String("server", "grpc"))
-			if err := pcepSession.SendPCUpdate(lspData.GetPolicyName(), plspId, labels); err != nil {
-				return &pb.LspStatus{IsSuccess: false}, err
-			}
-		} else {
-			if err := pcepSession.SendPCInitiate(lspData.GetPolicyName(), labels, lspData.GetColor(), uint32(100), lspData.GetSrcAddr(), lspData.GetDstAddr()); err != nil {
-				return &pb.LspStatus{IsSuccess: false}, err
-			}
-		}
-		return &pb.LspStatus{IsSuccess: true}, nil
-	}
-	return &pb.LspStatus{IsSuccess: false}, nil
-}
-
-func (s *Server) GetPeerAddrList(context.Context, *empty.Empty) (*pb.PeerAddrList, error) {
-	s.logger.Info("Get request GetPeerAddrList API", zap.String("server", "grpc"))
-	var ret pb.PeerAddrList
-	for _, pcepSession := range s.sessionList {
-		ret.PeerAddrs = append(ret.PeerAddrs, []byte(pcepSession.peerAddr))
-	}
-	return &ret, nil
-}
-
-func (s *Server) GetLspList(context.Context, *empty.Empty) (*pb.LspList, error) {
-	s.logger.Info("Get request GetSrPolicyList API", zap.String("server", "grpc"))
-	var ret pb.LspList
-	for _, lsp := range s.lspList {
-		lspData := &pb.LspData{
-			PcepSessionAddr: []byte(lsp.peerAddr),
-			Labels:          []*pb.Label{},
-			PolicyName:      lsp.name,
-			SrcAddr:         []byte(lsp.srcAddr),
-			DstAddr:         []byte(lsp.dstAddr),
-		}
-		for _, sid := range lsp.path {
-			label := pb.Label{
-				Sid: sid,
-			}
-			lspData.Labels = append(lspData.Labels, &label)
-		}
-		ret.Lsps = append(ret.Lsps, lspData)
-	}
-	return &ret, nil
-}
-
-func (s *Server) GetTed(context.Context, *empty.Empty) (*pb.Ted, error) {
-	s.logger.Info("Get request GetTed API", zap.String("server", "grpc"))
-	ret := &pb.Ted{
-		Enable: true,
-	}
-	if s.ted == nil {
-		ret.Enable = false
-		return ret, nil
-	}
-
-	for _, lsNodes := range s.ted.Nodes {
-		for _, lsNode := range lsNodes {
-			node := &pb.LsNode{
-				Asn:        lsNode.Asn,
-				RouterId:   lsNode.RouterId,
-				IsisAreaId: lsNode.IsisAreaId,
-				Hostname:   lsNode.Hostname,
-				SrgbBegin:  lsNode.SrgbBegin,
-				SrgbEnd:    lsNode.SrgbEnd,
-				LsLinks:    []*pb.LsLink{},
-				LsPrefixes: []*pb.LsPrefix{},
-			}
-			for _, lsLink := range lsNode.Links {
-				link := &pb.LsLink{
-					LocalRouterId:  lsLink.LocalNode.RouterId,
-					LocalAsn:       lsLink.LocalNode.Asn,
-					LocalIp:        lsLink.LocalIP.String(),
-					RemoteRouterId: lsLink.RemoteNode.RouterId,
-					RemoteAsn:      lsLink.RemoteNode.Asn,
-					RemoteIp:       lsLink.RemoteIP.String(),
-					Metrics:        []*pb.Metric{},
-					AdjSid:         lsLink.AdjSid,
-				}
-				for _, metric := range lsLink.Metrics {
-					metric := &pb.Metric{
-						Type:  pb.MetricType(pb.MetricType_value[metric.Type.String()]),
-						Value: metric.Value,
-					}
-					link.Metrics = append(link.Metrics, metric)
-				}
-				node.LsLinks = append(node.LsLinks, link)
-			}
-			for _, lsPrefix := range lsNode.Prefixes {
-				prefix := &pb.LsPrefix{
-					Prefix:   lsPrefix.Prefix.String(),
-					SidIndex: lsPrefix.SidIndex,
-				}
-				node.LsPrefixes = append(node.LsPrefixes, prefix)
-			}
-			ret.LsNodes = append(ret.LsNodes, node)
-		}
-	}
-	return ret, nil
-}
-
 func (s *Server) removeSession(session *Session) {
 	// Remove Session List
 	for i, v := range s.sessionList {
@@ -281,7 +147,7 @@ func (s *Server) removeSession(session *Session) {
 	s.lspList = newLspList
 }
 
-func (s *Server) getPlspId(lspData *pb.LspData) uint32 {
+func (s *Server) getPlspId(lspData *pb.SrPolicy) uint32 {
 	for _, v := range s.lspList {
 		if v.name == lspData.GetPolicyName() && v.peerAddr.Equal(net.IP(lspData.GetPcepSessionAddr())) {
 			return v.plspId
