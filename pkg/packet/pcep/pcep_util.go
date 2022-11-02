8e86cd7f516ea2795b0fbcae1b828c506360b230
watal <watal.i27e@gmail.com>
2022-11-02 21:06:19 +0900

    Add copyright

--- pkg/packet/pcep/pcep_util.go
+++ pkg/packet/pcep/pcep_util.go
@@ -1,3 +1,8 @@
+// Copyright (c) 2022 NTT Communications Corporation
+//
+// This software is released under the MIT License.
+// see https://github.com/nttcom/pola/blob/main/LICENSE
+
 package pcep
 
 import "encoding/binary"
