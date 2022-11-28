8df96d069da51a8a1cdc8479ad3a6d8a111e2bf5
Motok1 <m.takenaka@ntt.com>
2022-11-28 06:14:31 +0000

    v1.1.2

--- internal/pkg/version/version.go
+++ internal/pkg/version/version.go
@@ -9,7 +9,7 @@ import "fmt"
 
 const MAJOR uint = 1
 const MINOR uint = 1
-const PATCH uint = 1
+const PATCH uint = 2
 
 func Version() string {
 	return fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
