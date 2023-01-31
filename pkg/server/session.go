170610e3907a6e5f15bd79eb3facc78e4bda734f
Motok1 <m.takenaka@ntt.com>
2023-01-31 14:34:16 +0000

    rename receiver of "Session"

--- pkg/server/session.go
+++ pkg/server/session.go
@@ -32,33 +32,33 @@ type Session struct {
 }
 
 func NewSession(sessionId uint8, logger *zap.Logger) *Session {
-	s := &Session{
+	ss := &Session{
 		sessionId: sessionId,
 		isSynced:  false,
 		srpIdHead: uint32(1),
 		logger:    logger,
 		pccType:   pcep.RFC_COMPLIANT,
 	}
-	return s
+	return ss
 }
 
-func (s *Session) Established() {
-	if err := s.Open(); err != nil {
-		s.logger.Info("PCEP OPEN error", zap.String("session", s.peerAddr.String()), zap.Error(err))
+func (ss *Session) Established() {
+	if err := ss.Open(); err != nil {
+		ss.logger.Info("PCEP OPEN error", zap.String("session", ss.peerAddr.String()), zap.Error(err))
 		return
 	}
-	s.logger.Info("PCEP session established")
+	ss.logger.Info("PCEP session established")
 
-	if err := s.SendKeepalive(); err != nil {
-		s.logger.Info("Keepalive send error", zap.String("session", s.peerAddr.String()), zap.Error(err))
+	if err := ss.SendKeepalive(); err != nil {
+		ss.logger.Info("Keepalive send error", zap.String("session", ss.peerAddr.String()), zap.Error(err))
 		return
 	}
 
 	closeChan := make(chan bool)
 	defer close(closeChan)
 	go func() {
-		if err := s.ReceivePcepMessage(); err != nil {
-			s.logger.Info("Receive PCEP error", zap.String("session", s.peerAddr.String()))
+		if err := ss.ReceivePcepMessage(); err != nil {
+			ss.logger.Info("Receive PCEP error", zap.String("session", ss.peerAddr.String()))
 		}
 		closeChan <- true
 	}()
@@ -70,28 +70,28 @@ func (s *Session) Established() {
 		case <-closeChan:
 			return
 		case <-ticker.C: // pass KEEPALIVE seconds
-			if err := s.SendKeepalive(); err != nil {
-				s.logger.Info("Keepalive send error", zap.String("session", s.peerAddr.String()), zap.Error(err))
+			if err := ss.SendKeepalive(); err != nil {
+				ss.logger.Info("Keepalive send error", zap.String("session", ss.peerAddr.String()), zap.Error(err))
 			}
 		}
 	}
 }
 
-func (s *Session) Open() error {
-	if err := s.ReceiveOpen(); err != nil {
+func (ss *Session) Open() error {
+	if err := ss.ReceiveOpen(); err != nil {
 		return err
 	}
 
-	if err := s.SendOpen(); err != nil {
+	if err := ss.SendOpen(); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *Session) ReceiveOpen() error {
+func (ss *Session) ReceiveOpen() error {
 	// Parse CommonHeader
 	byteOpenHeader := make([]uint8, pcep.COMMON_HEADER_LENGTH)
-	if _, err := s.tcpConn.Read(byteOpenHeader); err != nil {
+	if _, err := ss.tcpConn.Read(byteOpenHeader); err != nil {
 		return err
 	}
 
@@ -105,11 +105,11 @@ func (s *Session) ReceiveOpen() error {
 		return fmt.Errorf("this peer has not been opened (messageType: %d)", openHeader.MessageType)
 	}
 
-	s.logger.Info("Receive Open", zap.String("session", s.peerAddr.String()))
+	ss.logger.Info("Receive Open", zap.String("session", ss.peerAddr.String()))
 
 	// Parse objectClass
 	byteOpenObject := make([]uint8, openHeader.MessageLength-pcep.COMMON_HEADER_LENGTH)
-	if _, err := s.tcpConn.Read(byteOpenObject); err != nil {
+	if _, err := ss.tcpConn.Read(byteOpenObject); err != nil {
 		return err
 	}
 
@@ -128,10 +128,10 @@ func (s *Session) ReceiveOpen() error {
 		}
 		for i, v := 0, openObjTlv.Value; i < int(openObjTlv.Length)/2; i++ { //
 			if binary.BigEndian.Uint16(v) == uint16(20) { // Cisco specific Assoc-Type
-				s.pccType = pcep.CISCO_LEGACY
+				ss.pccType = pcep.CISCO_LEGACY
 				break
 			} else if binary.BigEndian.Uint16(v) == uint16(65505) { // Juniper specific Assoc-Type
-				s.pccType = pcep.JUNIPER_LEGACY
+				ss.pccType = pcep.JUNIPER_LEGACY
 				break
 			}
 			v = v[2:]
@@ -140,33 +140,33 @@ func (s *Session) ReceiveOpen() error {
 	return nil
 }
 
-func (s *Session) SendOpen() error {
-	openMessage := pcep.NewOpenMessage(s.sessionId, KEEPALIVE)
+func (ss *Session) SendOpen() error {
+	openMessage := pcep.NewOpenMessage(ss.sessionId, KEEPALIVE)
 	byteOpenMessage := openMessage.Serialize()
 
-	s.logger.Info("Send Open", zap.String("session", s.peerAddr.String()))
-	if _, err := s.tcpConn.Write(byteOpenMessage); err != nil {
-		s.logger.Info("Open send error", zap.String("session", s.peerAddr.String()))
+	ss.logger.Info("Send Open", zap.String("session", ss.peerAddr.String()))
+	if _, err := ss.tcpConn.Write(byteOpenMessage); err != nil {
+		ss.logger.Info("Open send error", zap.String("session", ss.peerAddr.String()))
 		return err
 	}
 	return nil
 }
 
-func (s *Session) SendKeepalive() error {
+func (ss *Session) SendKeepalive() error {
 	keepaliveMessage := pcep.NewKeepaliveMessage()
 	byteKeepaliveMessage := keepaliveMessage.Serialize()
 
-	s.logger.Info("Send Keepalive", zap.String("session", s.peerAddr.String()))
-	if _, err := s.tcpConn.Write(byteKeepaliveMessage); err != nil {
+	ss.logger.Info("Send Keepalive", zap.String("session", ss.peerAddr.String()))
+	if _, err := ss.tcpConn.Write(byteKeepaliveMessage); err != nil {
 		return err
 	}
 	return nil
 }
 
-func (s *Session) ReceivePcepMessage() error {
+func (ss *Session) ReceivePcepMessage() error {
 	for {
 		byteCommonHeader := make([]uint8, pcep.COMMON_HEADER_LENGTH)
-		if _, err := s.tcpConn.Read(byteCommonHeader); err != nil {
+		if _, err := ss.tcpConn.Read(byteCommonHeader); err != nil {
 			return err
 		}
 		var commonHeader pcep.CommonHeader
@@ -174,11 +174,11 @@ func (s *Session) ReceivePcepMessage() error {
 
 		switch commonHeader.MessageType {
 		case pcep.MT_KEEPALIVE:
-			s.logger.Info("Received Keepalive", zap.String("session", s.peerAddr.String()))
+			ss.logger.Info("Received Keepalive", zap.String("session", ss.peerAddr.String()))
 		case pcep.MT_REPORT:
-			s.logger.Info("Received PCRpt", zap.String("session", s.peerAddr.String()))
+			ss.logger.Info("Received PCRpt", zap.String("session", ss.peerAddr.String()))
 			bytePcrptMessageBody := make([]uint8, commonHeader.MessageLength-pcep.COMMON_HEADER_LENGTH)
-			if _, err := s.tcpConn.Read(bytePcrptMessageBody); err != nil {
+			if _, err := ss.tcpConn.Read(bytePcrptMessageBody); err != nil {
 				return err
 			}
 			pcrptMessage := pcep.NewPCRptMessage()
@@ -187,39 +187,39 @@ func (s *Session) ReceivePcepMessage() error {
 			}
 			if pcrptMessage.LspObject.SFlag {
 				// During LSP state synchronization (RFC8231 5.6)
-				srPolicy := pcrptMessage.ToSRPolicy(s.pccType)
-				s.logger.Info("Synchronize SR Policy information", zap.String("session", s.peerAddr.String()), zap.Any("SRPolicy", srPolicy), zap.Any("Message", pcrptMessage))
-				go s.RegisterSRPolicy(srPolicy)
+				srPolicy := pcrptMessage.ToSRPolicy(ss.pccType)
+				ss.logger.Info("Synchronize SR Policy information", zap.String("session", ss.peerAddr.String()), zap.Any("SRPolicy", srPolicy), zap.Any("Message", pcrptMessage))
+				go ss.RegisterSRPolicy(srPolicy)
 			} else if !pcrptMessage.LspObject.SFlag {
 				if pcrptMessage.LspObject.PlspId == 0 {
 					// End of synchronization (RFC8231 5.6)
-					s.logger.Info("Finish PCRpt state synchronization", zap.String("session", s.peerAddr.String()))
-					s.isSynced = true
+					ss.logger.Info("Finish PCRpt state synchronization", zap.String("session", ss.peerAddr.String()))
+					ss.isSynced = true
 				} else if pcrptMessage.SrpObject.SrpId != 0 {
 					// Response to PCInitiate/PCUpdate (RFC8231 7.2)
-					srPolicy := pcrptMessage.ToSRPolicy(s.pccType)
-					s.logger.Info("Finish Stateful PCE request", zap.String("session", s.peerAddr.String()), zap.Uint32("srpId", pcrptMessage.SrpObject.SrpId))
-					go s.RegisterSRPolicy(srPolicy)
+					srPolicy := pcrptMessage.ToSRPolicy(ss.pccType)
+					ss.logger.Info("Finish Stateful PCE request", zap.String("session", ss.peerAddr.String()), zap.Uint32("srpId", pcrptMessage.SrpObject.SrpId))
+					go ss.RegisterSRPolicy(srPolicy)
 				}
 				// TODO: Need to implementation of PCUpdate for Passive stateful PCE
 			}
 		case pcep.MT_ERROR:
-			s.logger.Info("Received PCErr", zap.String("session", s.peerAddr.String()))
+			ss.logger.Info("Received PCErr", zap.String("session", ss.peerAddr.String()))
 			// TODO: Display error details
 		case pcep.MT_CLOSE:
-			s.logger.Info("Received Close", zap.String("session", s.peerAddr.String()))
+			ss.logger.Info("Received Close", zap.String("session", ss.peerAddr.String()))
 			// Close session if get Close Message
 			return nil
 
 		default:
-			s.logger.Info("Received unsupported MessageType", zap.String("session", s.peerAddr.String()), zap.Uint8("MessageType", commonHeader.MessageType))
+			ss.logger.Info("Received unsupported MessageType", zap.String("session", ss.peerAddr.String()), zap.Uint8("MessageType", commonHeader.MessageType))
 		}
 	}
 }
 
-func (s *Session) SendPCInitiate(srPolicy table.SRPolicy) error {
+func (ss *Session) SendPCInitiate(srPolicy table.SRPolicy) error {
 
-	pcinitiateMessage, err := pcep.NewPCInitiateMessage(s.srpIdHead, srPolicy.Name, srPolicy.SegmentList, srPolicy.Color, srPolicy.Preference, srPolicy.SrcAddr, srPolicy.DstAddr, pcep.VendorSpecific(s.pccType))
+	pcinitiateMessage, err := pcep.NewPCInitiateMessage(ss.srpIdHead, srPolicy.Name, srPolicy.SegmentList, srPolicy.Color, srPolicy.Preference, srPolicy.SrcAddr, srPolicy.DstAddr, pcep.VendorSpecific(ss.pccType))
 	if err != nil {
 		return err
 	}
@@ -228,16 +228,16 @@ func (s *Session) SendPCInitiate(srPolicy table.SRPolicy) error {
 		return err
 	}
 
-	s.logger.Info("Send PCInitiate", zap.String("session", s.peerAddr.String()), zap.Uint32("srpId", s.srpIdHead), zap.Any("srPolicy", srPolicy))
-	if _, err := s.tcpConn.Write(bytePCInitiateMessage); err != nil {
+	ss.logger.Info("Send PCInitiate", zap.String("session", ss.peerAddr.String()), zap.Uint32("srpId", ss.srpIdHead), zap.Any("srPolicy", srPolicy))
+	if _, err := ss.tcpConn.Write(bytePCInitiateMessage); err != nil {
 		return err
 	}
-	s.srpIdHead += 1
+	ss.srpIdHead += 1
 	return nil
 }
 
-func (s *Session) SendPCUpdate(srPolicy table.SRPolicy) error {
-	pcupdateMessage, err := pcep.NewPCUpdMessage(s.srpIdHead, srPolicy.Name, srPolicy.PlspId, srPolicy.SegmentList)
+func (ss *Session) SendPCUpdate(srPolicy table.SRPolicy) error {
+	pcupdateMessage, err := pcep.NewPCUpdMessage(ss.srpIdHead, srPolicy.Name, srPolicy.PlspId, srPolicy.SegmentList)
 	if err != nil {
 		return err
 	}
@@ -246,33 +246,33 @@ func (s *Session) SendPCUpdate(srPolicy table.SRPolicy) error {
 		return err
 	}
 
-	s.logger.Info("Send PCUpdate", zap.String("session", s.peerAddr.String()), zap.Uint32("srpId", pcupdateMessage.SrpObject.SrpId), zap.Any("srPolicy", srPolicy))
-	if _, err := s.tcpConn.Write(bytePCUpdMessage); err != nil {
-		s.logger.Info("PCUpdate send error", zap.String("session", s.peerAddr.String()))
+	ss.logger.Info("Send PCUpdate", zap.String("session", ss.peerAddr.String()), zap.Uint32("srpId", pcupdateMessage.SrpObject.SrpId), zap.Any("srPolicy", srPolicy))
+	if _, err := ss.tcpConn.Write(bytePCUpdMessage); err != nil {
+		ss.logger.Info("PCUpdate send error", zap.String("session", ss.peerAddr.String()))
 		return err
 	}
-	s.srpIdHead += 1
+	ss.srpIdHead += 1
 	return nil
 }
 
-func (s *Session) RegisterSRPolicy(srPolicy table.SRPolicy) {
-	s.DeleteSRPolicy(srPolicy.PlspId)
-	s.srPolicies = append(s.srPolicies, srPolicy)
+func (ss *Session) RegisterSRPolicy(srPolicy table.SRPolicy) {
+	ss.DeleteSRPolicy(srPolicy.PlspId)
+	ss.srPolicies = append(ss.srPolicies, srPolicy)
 }
 
-func (s *Session) DeleteSRPolicy(plspId uint32) {
-	for i, v := range s.srPolicies {
+func (ss *Session) DeleteSRPolicy(plspId uint32) {
+	for i, v := range ss.srPolicies {
 		if plspId == v.PlspId {
-			s.srPolicies[i] = s.srPolicies[len(s.srPolicies)-1]
-			s.srPolicies = s.srPolicies[:len(s.srPolicies)-1]
+			ss.srPolicies[i] = ss.srPolicies[len(ss.srPolicies)-1]
+			ss.srPolicies = ss.srPolicies[:len(ss.srPolicies)-1]
 			break
 		}
 	}
 }
 
 // return (PLSP-ID, true) if SR Policy is registerd, otherwise return (0, false)
-func (s *Session) SearchSRPolicyPlspId(color uint32, endpoint netip.Addr) (uint32, bool) {
-	for _, v := range s.srPolicies {
+func (ss *Session) SearchSRPolicyPlspId(color uint32, endpoint netip.Addr) (uint32, bool) {
+	for _, v := range ss.srPolicies {
 		if color == v.Color && endpoint == v.DstAddr {
 			return v.PlspId, true
 		}
