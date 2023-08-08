b631391e6c6e56f2be8e00aaef33c7d1fd2355e2
kanaya516 <abcs516tk1@gmail.com>
2023-08-08 23:03:59 +0900

    Add grpc for deleting sr-policy
    Signed-off-by: kanaya516 <abcs516tk1@gmail.com>

--- cmd/pola/sr_policy_delete.go
+++ cmd/pola/sr_policy_delete.go
@@ -8,26 +8,19 @@ package main
 import (
 	"errors"
 	"fmt"
-	"net/netip"
 	"os"
 
+	pb "github.com/nttcom/pola/api/grpc"
 	"github.com/spf13/cobra"
 	yaml "gopkg.in/yaml.v2"
 
-	pb "github.com/nttcom/pola/api/grpc"
 	"github.com/nttcom/pola/cmd/pola/grpc"
 )
 
-func newSRPolicyAddCmd() *cobra.Command {
-	srPolicyAddCmd := &cobra.Command{
+func newSRPolicyDeleteCmd() *cobra.Command {
+	srPolicyDeleteCmd := &cobra.Command{
 		Use: "add",
 		RunE: func(cmd *cobra.Command, args []string) error {
-
-			noLinkStateFlag, err := cmd.Flags().GetBool("no-link-state")
-			if err != nil {
-				return fmt.Errorf("flag error")
-
-			}
 
 			filepath, err := cmd.Flags().GetString("file")
 			if err != nil {
@@ -47,54 +40,24 @@ func newSRPolicyAddCmd() *cobra.Command {
 				return fmt.Errorf("file \"%s\" can't open", filepath)
 
 			}
-			if err := addSRPolicy(InputData, jsonFmt, noLinkStateFlag); err != nil {
+			if err := deleteSRPolicy(InputData, jsonFmt); err != nil {
 				return err
 			}
 			return nil
 		},
 	}
 
-	srPolicyAddCmd.Flags().BoolP("no-link-state", "l", false, "add SR Policy without Link State")
-	srPolicyAddCmd.Flags().StringP("file", "f", "", "[mandatory] path to yaml formatted LSP information file")
+	srPolicyDeleteCmd.Flags().StringP("file", "f", "", "[mandatory] path to yaml formatted LSP information file")
 
-	return srPolicyAddCmd
+	return srPolicyDeleteCmd
 
 }
 
-// Unify with table.Segment
-type Segment struct {
-	Sid string `yaml:"sid"`
-}
-
-// Unify with table.SRPolciy
-type SRPolicy struct {
-	PcepSessionAddr netip.Addr `yaml:"pcepSessionAddr"`
-	SrcAddr         netip.Addr `yaml:"srcAddr"`
-	DstAddr         netip.Addr `yaml:"dstAddr"`
-	SrcRouterID     string     `yaml:"srcRouterID"`
-	DstRouterID     string     `yaml:"dstRouterID"`
-	Name            string     `yaml:"name"`
-	SegmentList     []Segment  `yaml:"segmentList"`
-	Color           uint32     `yaml:"color"`
-	Type            string     `yaml:"type"`
-	Metric          string     `yaml:"metric"`
-}
-
-type InputFormat struct {
-	SRPolicy SRPolicy `yaml:"srPolicy"`
-	Asn      uint32   `yaml:"asn"`
-}
-
-func addSRPolicy(input InputFormat, jsonFlag bool, noLinkStateFlag bool) error {
-	if noLinkStateFlag {
-		if err := addSRPolicyNoLinkState(input); err != nil {
-			return err
-		}
-	} else {
-		if err := addSRPolicyLinkState(input); err != nil {
-			return err
-		}
+func deleteSRPolicy(input InputFormat, jsonFlag bool) error {
+	if err := deleteSRPolicyLinkState(input); err != nil {
+		return err
 	}
+
 	if jsonFlag {
 		fmt.Printf("{\"status\": \"success\"}\n")
 	} else {
@@ -104,48 +67,7 @@ func addSRPolicy(input InputFormat, jsonFlag bool, noLinkStateFlag bool) error {
 	return nil
 }
 
-func addSRPolicyNoLinkState(input InputFormat) error {
-	if !input.SRPolicy.PcepSessionAddr.IsValid() || input.SRPolicy.Color == 0 || !input.SRPolicy.SrcAddr.IsValid() || !input.SRPolicy.DstAddr.IsValid() || len(input.SRPolicy.SegmentList) == 0 {
-		sampleInput := "srPolicy:\n" +
-			"  pcepSessionAddr: 192.0.2.1\n" +
-			"  srcAddr: 192.0.2.1\n" +
-			"  dstAddr: 192.0.2.2\n" +
-			"  name: name\n" +
-			"  color: 100\n" +
-			"  segmentList:\n" +
-			"    - sid: 16003\n" +
-			"    - sid: 16002\n\n"
-
-		errMsg := "invalid input\n" +
-			"input examplse is below\n\n" +
-			sampleInput
-		return errors.New(errMsg)
-	}
-
-	segmentList := []*pb.Segment{}
-	for _, seg := range input.SRPolicy.SegmentList {
-		segmentList = append(segmentList, &pb.Segment{Sid: seg.Sid})
-	}
-	srPolicy := &pb.SRPolicy{
-		PcepSessionAddr: input.SRPolicy.PcepSessionAddr.AsSlice(),
-		SrcAddr:         input.SRPolicy.SrcAddr.AsSlice(),
-		DstAddr:         input.SRPolicy.DstAddr.AsSlice(),
-		SegmentList:     segmentList,
-		Color:           input.SRPolicy.Color,
-		PolicyName:      input.SRPolicy.Name,
-	}
-
-	inputData := &pb.CreateSRPolicyInput{
-		SRPolicy: srPolicy,
-	}
-	if err := grpc.CreateSRPolicyWithoutLinkState(client, inputData); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-func addSRPolicyLinkState(input InputFormat) error {
+func deleteSRPolicyLinkState(input InputFormat) error {
 	sampleInputDynamic := "#case: dynamic path\n" +
 		"asn: 65000\n" +
 		"srPolicy:\n" +
@@ -226,13 +148,12 @@ func addSRPolicyLinkState(input InputFormat) error {
 		SegmentList:     segmentList,
 		Metric:          metric,
 	}
-	inputData := &pb.CreateSRPolicyInput{
+	inputData := &pb.DeleteSRPolicyInput{
 		SRPolicy: srPolicy,
 		Asn:      input.Asn,
 	}
-	if err := grpc.CreateSRPolicy(client, inputData); err != nil {
+	if err := grpc.DeleteSRPolicy(client, inputData); err != nil {
 		return fmt.Errorf("gRPC Server Error: %s", err.Error())
-
 	}
 
 	return nil
