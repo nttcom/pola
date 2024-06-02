c8c46e6b8e1dd95c01d40283c5bc6711cbbfea4c
Motok1 <m.takenaka@ntt.com>
2024-06-02 15:12:40 +0000

    Set logging levels according to intent

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -12,12 +12,17 @@ import (
 	"go.uber.org/zap/zapcore"
 )
 
-func LogInit(fp *os.File) *zap.Logger {
+func LogInit(fp *os.File, dbg bool) *zap.Logger {
 	pe := zap.NewProductionEncoderConfig()
 	fileEncoder := zapcore.NewJSONEncoder(pe)
 	pe.EncodeTime = zapcore.ISO8601TimeEncoder
 	consoleEncoder := zapcore.NewConsoleEncoder(pe)
-	level := zap.InfoLevel
+	var level zapcore.Level
+	if dbg {
+		level = zap.DebugLevel
+	} else {
+		level = zap.InfoLevel
+	}
 	core := zapcore.NewTee(
 		zapcore.NewCore(fileEncoder, zapcore.AddSync(fp), level),
 		zapcore.NewCore(consoleEncoder, zapcore.AddSync(os.Stdout), level),
