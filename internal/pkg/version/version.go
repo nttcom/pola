22447c84ec8489c302f3c034fa4ea80df22e03a7
Motok1 <m.takenaka@ntt.com>
2024-06-06 08:35:28 +0000

    version 1.3.0-rc

--- internal/pkg/version/version.go
+++ internal/pkg/version/version.go
@@ -8,9 +8,9 @@ package version
 import "fmt"
 
 const MAJOR uint = 1
-const MINOR uint = 2
-const PATCH uint = 1
+const MINOR uint = 3
+const PATCH uint = 0
 
 func Version() string {
-	return fmt.Sprintf("%d.%d.%d", MAJOR, MINOR, PATCH)
+	return fmt.Sprintf("%d.%d.%d-rc", MAJOR, MINOR, PATCH)
 }
