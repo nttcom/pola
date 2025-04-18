dcd1ae78cb19a283be9cec249bf3442715f1fb92
watal <watal.i27e@gmail.com>
2025-04-18 10:14:03 +0900

    refactor,test(pcep): make Uint16ToByteSlice generic (~uint16) and add TLVType tests

--- pkg/packet/pcep/pcep_util.go
+++ pkg/packet/pcep/pcep_util.go
@@ -5,55 +5,50 @@
 
 package pcep
 
-import (
-	"encoding/binary"
+import "encoding/binary"
 
-	"golang.org/x/exp/constraints"
-)
+// AppendByteSlices concatenates multiple byte slices into a single slice.
+func AppendByteSlices(slices ...[]byte) []byte {
+	totalLength := 0
+	for _, s := range slices {
+		totalLength += len(s)
+	}
 
-// AppendByteSlices concatenates byte slices into a single slice.
-func AppendByteSlices(byteSlices ...[]byte) []byte {
-	// Calculate the total length of the joined slice.
-	joinedSliceLength := 0
-	for _, byteSlice := range byteSlices {
-		joinedSliceLength += len(byteSlice)
+	result := make([]byte, totalLength)
+	offset := 0
+	for _, s := range slices {
+		copy(result[offset:], s)
+		offset += len(s)
 	}
-	// Allocate the joined slice with the total length and copy the byte slices.
-	joinedSlice := make([]byte, joinedSliceLength)
-	var index int
-	for _, byteSlice := range byteSlices {
-		copy(joinedSlice[index:], byteSlice)
-		index += len(byteSlice)
-	}
-	return joinedSlice
+
+	return result
 }
 
-// Uint16ToByteSlice converts a uint16 value to a big-endian byte slice.
-func Uint16ToByteSlice(input uint16) []byte {
-	uint16Bytes := make([]byte, 2)
-	binary.BigEndian.PutUint16(uint16Bytes, input)
-	return uint16Bytes
+// Uint16ToByteSlice converts a uint16 or TLVType value to a big-endian byte slice.
+func Uint16ToByteSlice[T ~uint16](v T) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, uint16(v))
+	return b
 }
 
 // Uint32ToByteSlice converts a uint32 value to a big-endian byte slice.
-func Uint32ToByteSlice(input uint32) []byte {
-	uint32Bytes := make([]byte, 4)
-	binary.BigEndian.PutUint32(uint32Bytes, input)
-	return uint32Bytes
+func Uint32ToByteSlice(v uint32) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, v)
+	return b
 }
 
 // Bitwise is a type constraint for unsigned integer types (uint8, uint16, uint32).
 type Bitwise interface {
-	constraints.Unsigned
 	~uint8 | ~uint16 | ~uint32
 }
 
-// IsBitSet checks if a bit is set, with bit 0 as the least significant bit (LSB).
+// IsBitSet checks if a specific bit is set in the value, with bit 0 as the least significant bit (LSB).
 func IsBitSet[T Bitwise](value, mask T) bool {
 	return value&mask != 0
 }
 
-// SetBit sets a specific bit in a value of any unsigned integer type.
+// SetBit sets a specific bit in the value of any unsigned integer type.
 func SetBit[T Bitwise](value, bit T) T {
 	return value | bit
 }
