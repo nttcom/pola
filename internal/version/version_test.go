729e0f6542e939a3ea810aec65e6a69efb832973
watal <watal.i27e@gmail.com>
2026-08-14 20:29:33 +0900

    feat(version): add build metadata and embed release version

--- internal/version/version_test.go
+++ internal/version/version_test.go
@@ -6,13 +6,85 @@
 package version
 
 import (
-	"fmt"
+	"runtime"
+	"runtime/debug"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
-func TestVersion(t *testing.T) {
-	want := fmt.Sprintf("%d.%d.%d", Major, Minor, Patch)
-	assert.Equal(t, want, Version())
+func TestVersion_Default(t *testing.T) {
+	assert.Equal(t, devVersion, Version())
+}
+
+func TestVersion_Ldflags(t *testing.T) {
+	old := version
+	defer func() { version = old }()
+
+	version = "1.2.3"
+	assert.Equal(t, "1.2.3", Version())
+}
+
+func TestGet(t *testing.T) {
+	old := version
+	defer func() { version = old }()
+
+	version = "1.2.3"
+	info := Get()
+	assert.Equal(t, "1.2.3", info.Version)
+	assert.Equal(t, runtime.Version(), info.GoVersion)
+}
+
+func TestVcsSettings(t *testing.T) {
+	tests := []struct {
+		name         string
+		settings     []debug.BuildSetting
+		wantRevision string
+		wantTime     string
+		wantModified bool
+	}{
+		{
+			name: "vcs info present and clean",
+			settings: []debug.BuildSetting{
+				{Key: "vcs.revision", Value: "abc123"},
+				{Key: "vcs.time", Value: "2026-08-14T00:00:00Z"},
+				{Key: "vcs.modified", Value: "false"},
+			},
+			wantRevision: "abc123",
+			wantTime:     "2026-08-14T00:00:00Z",
+			wantModified: false,
+		},
+		{
+			name: "vcs info present and modified",
+			settings: []debug.BuildSetting{
+				{Key: "vcs.revision", Value: "abc123"},
+				{Key: "vcs.modified", Value: "true"},
+			},
+			wantRevision: "abc123",
+			wantModified: true,
+		},
+		{
+			name:         "no vcs info",
+			settings:     []debug.BuildSetting{{Key: "GOARCH", Value: "amd64"}},
+			wantRevision: "",
+			wantTime:     "",
+			wantModified: false,
+		},
+		{
+			name:         "no settings",
+			settings:     nil,
+			wantRevision: "",
+			wantTime:     "",
+			wantModified: false,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			revision, ts, modified := vcsSettings(tt.settings)
+			assert.Equal(t, tt.wantRevision, revision)
+			assert.Equal(t, tt.wantTime, ts)
+			assert.Equal(t, tt.wantModified, modified)
+		})
+	}
 }
